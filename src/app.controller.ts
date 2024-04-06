99e697453254fbf6396f1d669c613840dec3736b
EeeasyCode <eeeasycode@gmail.com>
2024-04-06 23:18:11 +0900

    feat: 이미지 업로드 구현 준비

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @ToDo : 이미지 업로드 기능 구현
   @Get('hello')
   getHello(): string {
     return this.appService.getHello();
