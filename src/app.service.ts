19407003e7029d541c316ee3ad0ec4d1382a34e5
Khan / 이창민 <khan.lee@wclub.co.kr>
2024-05-17 14:28:25 +0900

    feat: branch 변경

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { S3Client } from '@aws-sdk/client-s3';
-import { ConfigService } from '@nestjs/config';
-
-@Injectable()
-export class AppService {
-  constructor(private configService: ConfigService) {}
-}
